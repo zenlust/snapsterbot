625bf065a7ec43c234523bcea5c070ab2f75d381
zenlust <gertuyili@Gmail.com>
2024-10-22 16:11:40 +0700

    Add files via upload

---
+++ src/headers.py
@@ -0,0 +1,14 @@
+headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+    "if-none-match": 'W/"2cc-L/KMGs6EOmGfZznp6UXxDllJgL4"',
+    "referer": "https://prod.snapster.bot/main",
+    "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, seperti Gecko) Chrome/128.0.0.0 Safari/537.36",
+}
